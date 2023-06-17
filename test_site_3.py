0854c3097b939aca57d6718dae765da2ac706ac4
lera23me <136721645+lera23me@users.noreply.github.com>
2023-06-18 00:45:55 +0300

    Add files via upload

--- test_site_3.py
+++ test_site_3.py
@@ -8,32 +8,39 @@ URL = 'https://www.saucedemo.com/'
 USER_NAME = 'standard_user'
 PASSWORD = 'secret_sauce'
 
+
 def get_driver():
-        chrome_options = Options()
-        chrome_options.add_argument("--windows-size=1920,800")
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-        driver.implicitly_wait(10)
-        return driver
+    chrome_options = Options()
+    chrome_options.add_argument("--windows-size=1920,800")
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    driver.implicitly_wait(10)
+    return driver
+
 
 def open_page(driver, url):
     driver.get(url)
 
+
 def get_element_by_id(driver, locator):
     return driver.find_element(By.ID, locator)
+
 
 def element_click(driver, locator):
     element = get_element_by_id(driver, locator)
     element.click()
 
+
 def element_send_keys(driver, locator, text):
     element = get_element_by_id(driver, locator)
     element.send_keys(text)
+
 
 def login(driver, username, password):
     element_send_keys(driver, 'user-name', username)
     element_send_keys(driver, 'password', password)
     element_click(driver, 'login-button')
+
 
 driver = get_driver()
 open_page(driver, URL)
